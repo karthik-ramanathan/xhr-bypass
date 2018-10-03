22b8a180c2969ca2b84f89f248a3c293e9d99ea8
Karthik Ramanathan <karthik.r.ramanathan@ril.com>
2018-10-03 20:19:48 +0530

    Added get and post methods

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const request = require("request");
+
+const XHRProxy = {
+    GET(config, callback){
+        request({
+            url: config.url, 
+            strictSSL: false,
+            method: "GET",
+            json: true,
+            }, function(error, response, body){
+            typeof callback === "function" && callback(error, body);
+        });
+    },
+    POST(config, callback){
+        request({
+            url: config.url, 
+            strictSSL: false,
+            method: "POST",
+            json: true,
+            body: config.body
+            }, function(error, response, body){
+            typeof callback === "function" && callback(error, body);
+        });
+    }
+};
+
+export default XHRProxy;
